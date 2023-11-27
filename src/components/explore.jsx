8c76e7e6d2d2b4586c1fbc86e7b05b08999425cb
shadatr <shadadaab@gmail.com>
2023-11-27 14:36:11 +0300

    my posts added

--- src/components/explore.jsx
+++ src/components/explore.jsx
@@ -5,12 +5,18 @@ import { BsPersonCircle } from "react-icons/bs";
 import axios from "axios";
 
 function Explore(props) {
-  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
+  const [activeTab, setActiveTab] = useState("Tab 1");
 
   useEffect(() => {
     try {
-      axios.get("/api/posts").then((res) => {
-        setPosts(res.data);
+      axios.get("/api/allPosts").then((res) => {
+        setAllPosts(res.data);
+        console.log(res.data);
+      });
+      axios.get("/api/myPosts").then((res) => {
+        setMyPosts(res.data);
         console.log(res.data);
       });
     } catch {
@@ -21,6 +27,10 @@ function Explore(props) {
   if (!props?.auth || !props.auth.name) {
     redirect("/");
   }
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
   function encodeBase64(buffer) {
     let binary = "";
@@ -36,9 +46,36 @@ function Explore(props) {
 
   return (
     <div className=" flex flex-col items-center justify-center w-[100%] min-h-[75vh]">
-      {posts
-        ? posts.map((post) => (
-            <a href={`/post/${post._id}`} className="flex flex-col gap-5 lg:w-[800px] sm:w-[300px] border border-gray p-10 rounded-[30px] m-10">
+      <div className="flex w-full justify-center items-center flex-col">
+        <div className="lg:text-sm sm:text-xsm flex flex-row space-x-4 p-5">
+          <button
+            onClick={() => handleTabClick("Tab 1")}
+            className={`py-2 px-4 rounded-md border w-[200px] ${
+              activeTab === "Tab 1"
+                ? "bg-lightRed text-blue border-blue"
+                : "bg-gray text-darkGray border-lightGray"
+            }`}
+          >
+           All Posts
+          </button>
+          <button
+            onClick={() => handleTabClick("Tab 2")}
+            className={`py-2 px-4 rounded-md border w-[200px] ${
+              activeTab === "Tab 2"
+                ? "bg-lightRed text-blue border-blue"
+                : "bg-gray text-darkGray border-lightGray"
+            }`}
+          >
+            My Posts
+          </button>
+        </div>
+      </div>
+      {activeTab=="Tab 1"
+        ? allPosts.map((post) => (
+            <a
+              href={`/post/${post._id}`}
+              className="flex flex-col gap-5 lg:w-[800px] sm:w-[300px] border border-gray p-10 rounded-[30px] m-10"
+            >
               <div className="flex gap-5 items-center">
                 <p className="">
                   <BsPersonCircle size="40" />
@@ -52,7 +89,43 @@ function Explore(props) {
                     image.data
                   )}`;
                   return (
-                    <img key={index} src={dataURL} alt={`Post ${index}`} className="w-[350px] rounded-[20px]" />
+                    <img
+                      key={index}
+                      src={dataURL}
+                      alt={`Post ${index}`}
+                      className="w-[350px] rounded-[20px]"
+                    />
+                  );
+                })}
+              </div>
+            </a>
+          ))
+        : ""}
+        {activeTab=="Tab 2"
+        ? myPosts.map((post) => (
+            <a
+              href={`/post/${post._id}`}
+              className="flex flex-col gap-5 lg:w-[800px] sm:w-[300px] border border-gray p-10 rounded-[30px] m-10"
+            >
+              <div className="flex gap-5 items-center">
+                <p className="">
+                  <BsPersonCircle size="40" />
+                </p>
+                <h1 className="text-sm font-bold">{post.user_name}</h1>
+              </div>
+              <p>{post.text}</p>
+              <div className="grid grid-cols-2">
+                {post.images.map((image, index) => {
+                  const dataURL = `data:image/jpeg;base64,${encodeBase64(
+                    image.data
+                  )}`;
+                  return (
+                    <img
+                      key={index}
+                      src={dataURL}
+                      alt={`Post ${index}`}
+                      className="w-[350px] rounded-[20px]"
+                    />
                   );
                 })}
               </div>
